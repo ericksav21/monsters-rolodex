a24b2ae9e20fe466141b68cb58e2873e025ab3d4
Erick Alvarez Valencia <erickalv21@gmail.com>
2020-11-02 01:24:25 -0600

    Updates

---
+++ precache-manifest.2ed565cd5232a14005326a012e477d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7b1536f5d7d9decb7218ae80ad8924",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc6c7cf61ee994765d5c",
+    "url": "/monsters-rolodex/static/css/main.dc46ad70.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6c7cf61ee994765d5c",
+    "url": "/monsters-rolodex/static/js/main.756fee49.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
